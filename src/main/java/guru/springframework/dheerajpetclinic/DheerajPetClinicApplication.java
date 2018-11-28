be9d8ed931ade24e0aa64b052af92aa087ea71a6
Dheeraj DVRS <dheerajdvrs@gmail.com>
2018-11-28 19:05:54 +0530

    Initial commit

---
+++ src/main/java/guru/springframework/dheerajpetclinic/DheerajPetClinicApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.dheerajpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DheerajPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DheerajPetClinicApplication.class, args);
+	}
+}
